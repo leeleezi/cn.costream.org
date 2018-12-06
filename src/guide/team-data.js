f8b767d146cb2dee59321563e54e605c23ad5fbc
GalaxyYan <44971304+GalaxyYan@users.noreply.github.com>
2018-12-06 23:17:33 +0800

    Update by liyan

--- src/guide/team-data.js
+++ src/guide/team-data.js
@@ -358,7 +358,7 @@ team = team.concat(shuffle([ //shuffle 函数打乱了后续成员的顺序
 			org: 'HUST DML'
 		},
 		reposOfficial: [
-			'COStream - 前端',
+			'COStream - 前端'
 		],
 		links: [
 			'https://github.com/GalaxyYan'
